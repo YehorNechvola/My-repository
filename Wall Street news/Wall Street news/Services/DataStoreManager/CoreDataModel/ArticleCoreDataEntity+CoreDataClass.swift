59086515b233caac71e51c7ed0e20cb61f9b4a19
YehorNechvola <dp210495nes@gmail.com>
2022-07-31 18:28:21 +0300

    Created CoreDataManager

---
+++ Wall Street news/Wall Street news/Services/DataStoreManager/CoreDataModel/ArticleCoreDataEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleCoreDataEntity+CoreDataClass.swift
+//  Wall Street news
+//
+//  Created by Егор on 31.07.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class ArticleCoreDataEntity: NSManagedObject {
+
+}
