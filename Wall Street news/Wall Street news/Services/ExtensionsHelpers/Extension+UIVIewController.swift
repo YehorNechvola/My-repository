cd0dcfff4aa772271039a0ed8b8fbfe70ee76999
YehorNechvola <dp210495nes@gmail.com>
2022-09-08 11:55:25 +0300

    made checking of Internet connection and show alert when its false

---
+++ Wall Street news/Wall Street news/Services/ExtensionsHelpers/Extension+UIVIewController.swift
@@ -0,0 +1,8 @@
+//
+//  Extension+UIVIewController.swift
+//  Wall Street news
+//
+//  Created by Егор on 05.09.2022.
+//
+
+import Foundation
---
+++ Wall Street news/Wall Street news/Services/NetworkService/NetworkMonitor.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkMonitor.swift
+//  Wall Street news
+//
+//  Created by Егор on 01.09.2022.
+//
+
+import Foundation
